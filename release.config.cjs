2f3712884465a033fbb8fe854059919469fe9332
Kevin RodBe <kevin.rodriguez992@gmail.com>
2024-08-07 23:58:30 -0500

    ci: add semantic-release

---
+++ release.config.cjs
@@ -0,0 +1,53 @@
+module.exports = {
+  branches: [
+    '+([0-9])?(.{+([0-9]),x}).x',
+    'main',
+    'next',
+    'next-major',
+    {
+      name: 'beta',
+      prerelease: true,
+    },
+    {
+      name: 'alpha',
+      prerelease: true,
+    },
+    {
+      name: 'pre',
+      prerelease: true,
+    },
+  ],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    [
+      '@semantic-release/release-notes-generator',
+      {
+        preset: 'conventionalcommits',
+        parserOpts: {
+          noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES', 'BREAKING'],
+        },
+        presetConfig: {
+          changelogHeader:
+            '# Hi\n\n bais hi lorem ipsum ![Insomnia API Client](https://raw.githubusercontent.com/Kong/insomnia/develop/screenshots/main.png)',
+          compareUrlFormat: '{{host}}/{{owner}}/{{repository}}/compare/{{previousTag}}...{{currentTag}}',
+          issueUrlFormat: '{{host}}/{{owner}}/{{repository}}/bug/{{id}}',
+          issuePrefixes: ['#'],
+        },
+      },
+    ],
+    '@semantic-release/changelog',
+    [
+      '@semantic-release/npm',
+      {
+        npmPublish: true,
+      },
+    ],
+    [
+      '@semantic-release/git',
+      {
+        message: 'release: ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+    '@semantic-release/github',
+  ],
+};
