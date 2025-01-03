1f616b9046790ac26aeda357ef9f7117a0dd3f34
kevinrodbe <kevinrodbe@users.noreply.github.com>
2025-01-03 01:16:58 -0500

    test: fix typo func

--- src/utils/fs.test.ts
+++ src/utils/fs.test.ts
@@ -1,11 +1,11 @@
 import { describe, expect, it } from 'vitest';
 
-import { readFile } from './fs';
+import { readJsonFile } from './fs';
 
-describe('readFile', () => {
+describe('readJsonFile', () => {
   it('should read file with empty file', () => {
     const filePath = `${process.cwd()}/empty.json`;
-    const result = readFile(filePath);
+    const result = readJsonFile(filePath);
     console.log(result);
     expect(result).toEqual(null);
   });
@@ -14,7 +14,7 @@ describe('readFile', () => {
 describe('getAllScriptsFromPackageJsons', () => {
   it('should read prop name from package.json', () => {
     const filePath = `${process.cwd()}/package.json`;
-    const result = readFile(filePath);
+    const result = readJsonFile(filePath);
 
     expect(result.name).toBe('@rodbe/nsl');
   });
