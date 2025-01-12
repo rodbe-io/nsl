e00980949ca0ab675a2f331c8c42585517a94542
kevinrodbe <kevinrodbe@users.noreply.github.com>
2025-01-12 00:31:08 -0500

    feat: check new nsl version

--- src/tasks/update.ts
+++ src/tasks/update.ts
@@ -1,27 +1,11 @@
 import { execSync } from 'node:child_process';
 
-import select from '@inquirer/select';
+import chalk from 'chalk';
 import fetch from 'node-fetch';
 
 import { cacheFactory } from '@/adapters/cache';
-import { SHORT_CONFIG_CACHE_NAME, LONG_CONFIG_CACHE_NAME, STATUS, DAY_IN_MS, WEEK_IN_MS } from '@/constants';
+import { SHORT_CONFIG_CACHE_NAME, STATUS, DAY_IN_MS } from '@/constants';
 import { getNslPkgJson } from '@/helpers/nsl';
-import { logNslBanner } from '@/helpers/log';
-
-const updateOptions = {
-  message: 'Heey! Before, do you want to get the latest version? 🔥',
-  default: true,
-  choices: [
-    {
-      name: 'Yes',
-      value: true,
-    },
-    {
-      name: 'No',
-      value: false,
-    },
-  ],
-};
 
 const commandToInstallNsl = 'npm i -g @rodbe/nsl';
 
@@ -29,11 +13,6 @@ const shortConfigCache = cacheFactory<string, any>({
   max: 10,
   ttl: DAY_IN_MS,
   cacheName: SHORT_CONFIG_CACHE_NAME,
-});
-const longConfigCache = cacheFactory<string, any>({
-  max: 10,
-  ttl: WEEK_IN_MS,
-  cacheName: LONG_CONFIG_CACHE_NAME,
 });
 
 type NpmPackage = {
@@ -55,33 +34,22 @@ export const update = async () => {
     stdio: [process.stdin, process.stdout, process.stderr],
   });
 
-  longConfigCache.setCache('status', STATUS.UPDATED);
-  shortConfigCache.deleteCache('dontAsk');
+  shortConfigCache.setCache('status', STATUS.UPDATED);
 };
 
 export const checkAvailableUpdate = async () => {
-  if (longConfigCache.getCache('status') === STATUS.UPDATED) {
-    return;
-  }
-
-  if (shortConfigCache.getCache('dontAsk')) {
+  if (shortConfigCache.getCache('status') === STATUS.UPDATED) {
     return;
   }
 
   const remotePkgJson = await getRemotePackageJson(getNslPkgJson().name);
-
   if (remotePkgJson.version === getNslPkgJson().version) {
-    longConfigCache.setCache('status', STATUS.UPDATED);
+    shortConfigCache.setCache('status', STATUS.UPDATED);
+
     return;
   }
 
-  logNslBanner();
-  const updateAnswer = await select(updateOptions);
-
-  if (updateAnswer) {
-    await update();
-  } else {
-    shortConfigCache.setCache('dontAsk', true);
-    longConfigCache.deleteCache('status');
-  }
+  console.log(chalk.black.bold.bgGreenBright('Downloading latest version 🔥🔥🔥'));
+  shortConfigCache.deleteCache('status');
+  await update();
 };
